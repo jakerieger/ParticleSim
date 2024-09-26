5376cc5c3442a3cab45f4bfbe5f886ba7a0e308a
Jake Rieger <contact.jakerieger@gmail.com>
2024-09-26 00:11:25 -0400

    updated OpenGLTK

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,69 +1,20 @@
+// Author: Jake Rieger
+// Created: 9/4/2024.
+//
 
 #include "Types.h"
 #include "OpenGLTK.h"
+#include "Glfw/GlfwApp.h"
 
-#include <Panic/Panic.h>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-using namespace GLTK;
-
-static constexpr i32 kWindowWidth = 800;
-static constexpr i32 kWindowHeight = 600;
-
-GLFWwindow* gWindow = None;
-Unique<Clock> gClock;
-
-void Update(f32 dT);
-void Render();
+class ParticleSim final : public GLTK::IGlfwApp {
+public:
+    ParticleSim(const str& title, int width, int height) : IGlfwApp(title, width, height) {}
+    void Update(f32 dT) override {}
+    void Render() override {}
+};
 
 int main() {
-    if (!glfwInit())
-        Panic("Failed to initialize GLFW");
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    gWindow = glfwCreateWindow(kWindowWidth, kWindowHeight, "ParticleSim (x64)", None, None);
-    if (!gWindow) {
-        glfwTerminate();
-        Panic("Failed to create GLFW window");
-    }
-
-    glfwMakeContextCurrent(gWindow);
-
-    if (!gladLoadGLLoader(RCAST<GLADloadproc>(glfwGetProcAddress))) {
-        glfwTerminate();
-        Panic("Failed to initialize OpenGL context");
-    }
-
-    glViewport(0, 0, kWindowWidth, kWindowHeight);
-    glClearColor(0.f, 0.f, 0.f, 1.f);
-
-    gClock = std::make_unique<Clock>();
-    gClock->Start();
-    while (!glfwWindowShouldClose(gWindow)) {
-        gClock->Tick();
-        glfwPollEvents();
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        Update(gClock->GetDeltaTime());
-        Render();
-
-        glfwSwapBuffers(gWindow);
-        gClock->Update();
-    }
-    gClock->Stop();
-
-    gClock.reset();
-    glfwDestroyWindow(gWindow);
-    glfwTerminate();
-
+    ParticleSim app("ParticleSim", 800, 600);
+    app.Run();
     return 0;
 }
-
-void Update(f32 dT) {
-}
-
-void Render() {}
